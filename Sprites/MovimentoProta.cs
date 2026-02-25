470883e9391b6065340e6f85fedc1d37340567b8
RUAN HAMES <ruan_hames@estudante.sesisenai.org.br>
2026-02-25 16:32:03 -0300

    criação do projeto

---
+++ Sprites/MovimentoProta.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class MovimentoProta : MonoBehaviour
+
+void public gameObject
+
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+if (Input.GetKeyDown(KeyCode.XX))
+
+
+    }
+}
